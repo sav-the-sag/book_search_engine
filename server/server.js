2d3b489c570c42aa2264388f3a2365ba5e7de7d0
savan <savannahwilliams243@gmail.com>
2024-02-28 10:15:00 -0800

    created async StartApolloServer fx and called it to start the server

--- server/server.js
+++ server/server.js
@@ -21,13 +21,36 @@ const server = new ApolloServer(
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
+// Serves client/build as static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+  // res.sendFile(path.join(__dirname, '../client/'));
 });
+
+// app.use(routes);
+
+/*** Use Apollo Server instead*/
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
+
+// Apollo starts, applies middleware, and waits for the db 
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// Call the async function to start the server
+startApolloServer();
