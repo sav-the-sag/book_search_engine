e14537b447e6fb6755fd8a655412489b852f8803
savan <savannahwilliams243@gmail.com>
2024-02-28 13:52:58 -0800

    added mutation for adding new users

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,3 +14,18 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+// Mutation for adding new users
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        password
+      }
+    }
+  }
+`;
