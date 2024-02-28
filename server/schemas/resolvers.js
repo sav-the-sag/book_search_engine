7c0858bdb723c5f12672a2267fd6987d68fd8a0e
savan <savannahwilliams243@gmail.com>
2024-02-28 10:18:51 -0800

    started working on resolvers, created query by getting single user with User.findOne

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,19 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    /* QUERIES */
+      Query: {
+        // Get a single user by ID or username
+        me: async (parent, args, context) => {
+          if (context.user) {
+            return await User.findOne(
+              { _id: context.user._id },
+              { __v: 0, password: 0 }
+            );
+          } 
+          throw new AuthenticationError('You need to log in...');
+        },
+      },
+    }
