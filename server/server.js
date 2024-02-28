2b06ef333f6e3d7a2c4e5bb361ededc08edf42a6
savan <savannahwilliams243@gmail.com>
2024-02-28 10:10:22 -0800

    imported ApolloServer, authMiddleware, typeDefs, and resolvers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer(
+  {
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+  }
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
