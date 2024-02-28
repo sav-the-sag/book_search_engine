e997a2c426da0921d4a37302455b0f4da7228738
savan <savannahwilliams243@gmail.com>
2024-02-28 10:20:19 -0800

    defined mutation type

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,16 +4,29 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     /* QUERIES */
-      Query: {
+    Query: {
         // Get a single user by ID or username
         me: async (parent, args, context) => {
-          if (context.user) {
-            return await User.findOne(
-              { _id: context.user._id },
-              { __v: 0, password: 0 }
-            );
-          } 
-          throw new AuthenticationError('You need to log in...');
+            if (context.user) {
+                return await User.findOne(
+                    { _id: context.user._id },
+                    { __v: 0, password: 0 }
+                );
+            }
+            throw new AuthenticationError('You need to log in...');
         },
-      },
+    },
+    /* MUTATIONS */
+    Mutation: {
+        // Create a new user
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+
+            //* console.log(user);
+            const token = signToken(user);
+
+            //* console.log({input});
+            return { token, user };
+        },
     }
+}
