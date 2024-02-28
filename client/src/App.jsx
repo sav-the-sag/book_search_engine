fcd9f4ccb2be1cad8be64caaec980570175561ac
savan <savannahwilliams243@gmail.com>
2024-02-28 14:01:03 -0800

    completed App() fx by providing Apollo Client, rendering navbar and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,22 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    // provides the Apollo Client instance
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          {/* renders Navbar component */}
+          <Navbar />
+
+          {/* sets up routes */}
+          <Routes>
+            <Route path='/' element={<SearchBooks />} /> {/* render the SearchBooks component */}
+            <Route path='/saved' element={<SavedBooks />} /> {/* render the SavedBooks component */}
+            <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} /> {/* render a default error message for wrong page */}
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
